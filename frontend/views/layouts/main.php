120bf30dbcd9c1df63d82dd4e3ed5fe872354f0a
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-03-20 15:39:22 +0300

    Подключение bootstrap 4 библиотек

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -4,10 +4,11 @@
 /* @var $content string */
 
 use yii\helpers\Html;
-use yii\bootstrap\Nav;
-use yii\bootstrap\NavBar;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
 use yii\widgets\Breadcrumbs;
 use frontend\assets\AppAsset;
+use yii\widgets\ActiveForm;
 use common\widgets\Alert;
 
 AppAsset::register($this);
@@ -32,7 +33,7 @@ AppAsset::register($this);
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar navbar-expand-lg navbar-light bg-light',
         ],
     ]);
     $menuItems = [
