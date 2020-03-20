855c9c6672b02ff114b094ff494744dcdba6c309
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-03-20 19:36:03 +0300

    Незначительное изменение 2

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -17,7 +17,12 @@ $this->title = 'My Yii Application';
     <div class="body-content">
         <div class="row">
             <div class="col-lg-6">
-                <h2>Heading</h2>
+                <h2>Заголовок</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
 
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
