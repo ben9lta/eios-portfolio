a99e9585d76b505bfd4e119681e4551a4af1385c
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-03-22 14:45:43 +0300

    Изменение текста
    cherry picked from commit 906fb01600256fec42c91b668e7035bf85be2c63)

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -23,14 +23,14 @@ class SignupForm extends Model
         return [
             ['username', 'trim'],
             ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
+            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'Этот логин уже используется.'],
             ['username', 'string', 'min' => 2, 'max' => 255],
 
             ['email', 'trim'],
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'Данный email уже используется.'],
 
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
@@ -73,7 +73,7 @@ class SignupForm extends Model
             )
             ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' robot'])
             ->setTo($this->email)
-            ->setSubject('Account registration at ' . Yii::$app->name)
+            ->setSubject('Регистрация на ' . Yii::$app->name)
             ->send();
     }
 }
