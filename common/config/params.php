ab3055ed9cfab2f5d222f86e5c32bb7b3ed4b89b
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-03-24 01:46:15 +0300

    Смена email для отправки ключа подтверждения

--- common/config/params.php
+++ common/config/params.php
@@ -1,8 +1,8 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-//    'supportEmail' => 'eios-test@mail.ru',
-    'supportEmail' => 'SergeNikiforov97@yandex.ru',
+    'supportEmail' => 'eios-test@mail.ru',
+//    'supportEmail' => 'SergeNikiforov97@yandex.ru',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
