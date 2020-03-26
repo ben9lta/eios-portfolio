52b0be0c5256bf39d593a6c90bbb5cb146cf41f8
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-03-26 20:23:37 +0300

    Добавление остальных полей в USER
    (cherry picked from commit e3157bbfe17edcfa28c78c585618d18ef66e1e25)
    (cherry picked from commit 1aa55cf01d72576ff8acace4230dbebf144aae47)

---
+++ console/migrations/m200326_172320_add_other_fields_to_user_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m200326_172320_add_other_fields_to_user_table
+ */
+class m200326_172320_add_other_fields_to_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn('user', 'phone', $this->string(11));
+        $this->addColumn('user', 'gender', $this->smallInteger()->notNull()->defaultValue(0));
+        $this->addColumn('user', 'birthday', $this->dateTime());
+        $this->addColumn('user', 'consent', $this->boolean()); // Согласие на обработку персональных данных
+        $this->addColumn('user', 'photo', $this->string());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn('user', 'phone');
+        $this->dropColumn('user', 'gender');
+        $this->dropColumn('user', 'birthday');
+        $this->dropColumn('user', 'consent');
+        $this->dropColumn('user', 'photo');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200326_172320_add_other_fields_to_user_table cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
