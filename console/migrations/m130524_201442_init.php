e3157bbfe17edcfa28c78c585618d18ef66e1e25
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-03-26 18:52:55 +0300

    Добавление полей в таблицу User

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -19,9 +19,14 @@ class m130524_201442_init extends Migration
             'password_hash'        => $this->string()->notNull(),
             'password_reset_token' => $this->string()->unique(),
             'email'                => $this->string()->notNull()->unique(),
-            'status'               => $this->smallInteger()->notNull()->defaultValue(0),
+            'status'               => $this->smallInteger()->notNull()->defaultValue(0), // Статус активности аккаунта 0 - не активен, 9 - ждет подтверждения, 10 - подтвержден
             'created_at'           => $this->integer()->notNull(),
             'updated_at'           => $this->integer()->notNull(),
+            'phone'                => $this->string(11),
+            'gender'               => $this->smallInteger()->notNull()->defaultValue(0), // 0 - не определено, 1 - мужской, 2 - женский
+            'birthday'             => $this->date()->notNull(),
+            'consent'              => $this->boolean()->notNull(), // Согласие на обработку данных
+            'photo'                => $this->string()->null(),
         ], $tableOptions);
     }
 
