e5eedadbebe8936923116b30276cca205a7aac00
ben9lta <33951956+ben9lta@users.noreply.github.com>
2020-03-30 22:25:27 +0300

    В таблице User свойство Gender установлено как null

--- console/migrations/m200326_172320_add_other_fields_to_user_table.php
+++ console/migrations/m200326_172320_add_other_fields_to_user_table.php
@@ -13,7 +13,7 @@ class m200326_172320_add_other_fields_to_user_table extends Migration
     public function safeUp()
     {
         $this->addColumn('user', 'phone', $this->string(11));
-        $this->addColumn('user', 'gender', $this->smallInteger()->notNull()->defaultValue(0));
+        $this->addColumn('user', 'gender', $this->smallInteger()->defaultValue(0));
         $this->addColumn('user', 'birthday', $this->dateTime());
         $this->addColumn('user', 'consent', $this->boolean()); // Согласие на обработку персональных данных
         $this->addColumn('user', 'photo', $this->string());
