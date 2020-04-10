87466f9b9fdbc47c570532d4930ddc2d17b077d5
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-04-11 00:30:52 +0300

    Изменено: - Отключены хлебные крошки на форме входа - Внешнее изменение формы входа

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -8,34 +8,38 @@ use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
 
 $this->title = 'Вход';
-$this->params['breadcrumbs'][] = $this->title;
+//$this->params['breadcrumbs'][] = $this->title;
+
+//$this->registerJs("jQuery('#reveal-password').change(function(){jQuery('#login-password-form').attr('type',this.checked?'text':'password');})");
+
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Пожалуйста, заполните следующие поля для входа:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    Если вы забыли пароль вы можете <?= Html::a('восстановить его', ['site/request-password-reset']) ?>.
-                    <br>
-                    Нужно новое письмо с подтверждением? <?= Html::a('Отрпавить', ['site/resend-verification-email']) ?>
-                </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Войти', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+    <div class="card card-login card-container">
+<!--        <h1 class="text-center">--><?//= Html::encode($this->title) ?><!--</h1>-->
+        <div class="thumbnail">
+    <!--        --><?//= Html::img('@common/images/hat.png', ['class' => 'profile-img-card my-4'])?>
+            <?= Html::img('https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/hat.svg', ['class' => 'profile-img-card my-4'])?>
         </div>
+        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+            <?= $form->field($model, 'username')->textInput(['autofocus' => true, 'placeholder' => 'Логин'])->label(false) ?>
+<!--            <div class="reveal-password">-->
+<!--                --><?//= Html::checkbox('reveal-password', false, ['id' => 'reveal-password']) ?>
+<!--                --><?//= Html::label('', ['for' => 'reveal-password']) ?>
+<!--            </div>-->
+            <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Пароль', 'id' => 'login-password-form'])->label(false) ?>
+
+            <?= $form->field($model, 'rememberMe')->checkbox() ?>
+            <div class="form-group">
+                <?= Html::submitButton('Войти', ['class' => 'btn btn-lg btn-red btn-block', 'name' => 'login-button']) ?>
+            </div>
+
+            <div style="color:#999;margin:1em 0">
+                <?= Html::a('Восстановить пароль.', ['site/request-password-reset'], ['class' => 'color-red']) ?>
+                <br>
+                <?= Html::a('Отрпавить письмо с подтверждением.', ['site/resend-verification-email'], ['class' => 'color-red']) ?>
+            </div>
+
+        <?php ActiveForm::end(); ?>
     </div>
 </div>
