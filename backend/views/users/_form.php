aa58ee7bd8b7118dd9afdd898fe6f4badd82aff1
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-04-18 03:32:44 +0300

    CRUD User. Backend: Определены поля для изменения данных пользователя

--- backend/views/users/_form.php
+++ backend/views/users/_form.php
@@ -1,11 +1,14 @@
 <?php
 
+use kartik\date\DatePicker;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\widgets\MaskedInput;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\users\Users */
 /* @var $form yii\widgets\ActiveForm */
+
 ?>
 
 <div class="users-form">
@@ -14,21 +17,21 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'auth_key')->textInput(['maxlength' => true]) ?>
+<!--    --><?//= $form->field($model, 'auth_key')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'password_hash')->textInput(['maxlength' => true]) ?>
+<!--    --><?//= $form->field($model, 'password_hash')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'password_reset_token')->textInput(['maxlength' => true]) ?>
+<!--    --><?//= $form->field($model, 'password_reset_token')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'status')->textInput() ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+<!--    --><?//= $form->field($model, 'created_at')->textInput() ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+<!--    --><?//= $form->field($model, 'updated_at')->textInput() ?>
 
-    <?= $form->field($model, 'verification_token')->textInput(['maxlength' => true]) ?>
+<!--    --><?//= $form->field($model, 'verification_token')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
 
@@ -36,13 +39,28 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'middle_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'phone')->textInput(['type' => 'number', 'maxlength' => true])->widget(MaskedInput::className(), [
+        'mask' => '+7 (999) 999-99-99',
+        'options' => [
+            'placeholder' => '+7 (999) 999-99-99'
+        ],
+    ]) ?>
 
-    <?= $form->field($model, 'gender')->textInput() ?>
+    <?= $form->field($model, 'gender')->dropDownList(['' => '', 1 => 'Мужской', 2 => 'Женский']) ?>
 
-    <?= $form->field($model, 'birthday')->textInput() ?>
-
-    <?= $form->field($model, 'consent')->textInput() ?>
+    <label class="control-label">Дата рождения</label>
+    <?= DatePicker::widget([
+        'name' => 'Users[birthday]',
+        'value' => $model->getBirthday(),
+        'options' => ['placeholder' => 'Выберите дату'],
+        'type' => DatePicker::TYPE_COMPONENT_APPEND,
+        'pluginOptions' => [
+            'format' => 'dd.mm.yyyy',
+            'todayHighlight' => true,
+            'autoclose' => true,
+        ]
+    ]); ?>
+    <p class="mb-3"></p>
 
     <?= $form->field($model, 'photo')->textInput(['maxlength' => true]) ?>
 
