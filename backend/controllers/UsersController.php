a27c6dbcacf814c5e6ee9eff725d01af5f708b11
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-04-24 13:35:41 +0300

    Права доступа к админке Users

--- backend/controllers/UsersController.php
+++ backend/controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace backend\controllers;
 use Yii;
 use backend\models\users\Users;
 use backend\models\UsersSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -24,6 +25,15 @@ class UsersController extends Controller
                 'class' => VerbFilter::className(),
                 'actions' => [
                     'delete' => ['POST'],
+                ],
+            ],
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['Администратор'],
+                    ],
                 ],
             ],
         ];
