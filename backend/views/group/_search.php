c52500f122bc291d2f519c7892b7a0f3a5a68a16
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-04-24 19:52:14 +0300

    Backend. CRUD Group

---
+++ backend/views/group/_search.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\GroupSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="group-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <?= $form->field($model, 'course') ?>
+
+    <?= $form->field($model, 'dir_id') ?>
+
+    <?= $form->field($model, 'curator_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Найти', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Очистить', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
