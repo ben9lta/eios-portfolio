0ea38b36a4f6baa0205a201b40fb73959901b302
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-04-24 20:06:12 +0300

    Backend. CRUD Department

---
+++ backend/views/department/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use common\modules\GridView\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel backend\models\DepartmentSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Факультеты';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="department-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Добавить', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+
+            'id',
+            'title',
+            'address',
+            'inst_id',
+
+            [
+                'class' => 'common\modules\GridView\ActionColumn',
+                'header'=>'Действия',
+            ],
+        ],
+    ]); ?>
+
+
+</div>
