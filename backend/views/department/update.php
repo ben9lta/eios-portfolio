68c088be435170c3ee8debe6a91c2c228b72dece
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-04-24 20:17:02 +0300

    Backend. CRUD Department

--- backend/views/department/update.php
+++ backend/views/department/update.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
 $this->title = 'Редактирование: ' . $model->title;
 $this->params['breadcrumbs'][] = ['label' => 'Факультеты', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Изменить';
+$this->params['breadcrumbs'][] = 'Редактирование';
 ?>
 <div class="department-update">
 
