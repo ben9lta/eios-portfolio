331b8adb89e6f494f444d2b654fdc4539dfed279
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-04-24 20:01:44 +0300

    Backend. CRUD Directions

---
+++ backend/views/direction/_search.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\DirectionSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="direction-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <?= $form->field($model, 'code') ?>
+
+    <?= $form->field($model, 'dep_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Поиск', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Очистить', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
