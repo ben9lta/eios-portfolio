090f4796e1f3a25bab349d3619908d593205d586
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-05-25 00:56:21 +0300

    Backend. Users: Перевод текста кнопок на форме

--- backend/views/users/_search.php
+++ backend/views/users/_search.php
@@ -52,8 +52,8 @@ use yii\widgets\ActiveForm;
     <?php // echo $form->field($model, 'photo') ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+        <?= Html::submitButton('Поиск', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Очистить', ['class' => 'btn btn-outline-secondary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
