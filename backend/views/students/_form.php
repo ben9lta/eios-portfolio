234f5bc5cce6a31eff7606a72fab51eb10d7cf7a
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-05-31 23:24:12 +0300

    Backend. Students: добавление виджета для выборки

--- backend/views/students/_form.php
+++ backend/views/students/_form.php
@@ -1,5 +1,7 @@
 <?php
 
+use kartik\select2\Select2;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -14,11 +16,29 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'id')->textInput() ?>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
+    <?=
+        $form->field($model, 'user_id')->widget(Select2::classname(), [
+            'data' => ArrayHelper::map(\common\models\User::find()->asArray()->all(), 'id', 'email'),
+            'options' => ['placeholder' => 'Выберите пользователя'],
+            'pluginOptions' => [
+                'allowClear' => true,
+                'minimumInputLength' => 0, //Кол-во символов для поиска и вывода информации
+            ],
+        ]);
+    ?>
 
-    <?= $form->field($model, 'group_id')->textInput() ?>
+    <?=
+    $form->field($model, 'group_id')->widget(Select2::classname(), [
+        'data' => ArrayHelper::map(\common\models\Group::find()->asArray()->all(), 'id', 'title'),
+        'options' => ['placeholder' => 'Выберите группу'],
+        'pluginOptions' => [
+            'allowClear' => true,
+            'minimumInputLength' => 0, //Кол-во символов для поиска и вывода информации
+        ],
+    ]);
+    ?>
 
-    <?= $form->field($model, 'budget')->textInput() ?>
+    <?= $form->field($model, 'budget')->dropDownList(['' => '', 0 => 'Коммерция', 1 => 'Бюджет']) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-success']) ?>
