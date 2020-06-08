2faa5a253132f8096a39f194fe30f1bd7a29a180
Alexandr Bondar <ya.sasha.bondar30@gmail.com>
2020-06-08 22:51:17 +0300

    Frontend. ViewStudentsEdu: Изменен вывод таблицы

--- frontend/views/students/section/edu.php
+++ frontend/views/students/section/edu.php
@@ -10,6 +10,8 @@ $this->title = 'Учебная деятельность';
 $this->params['breadcrumbs'][] = $this->title;
 
 use common\modules\GridView\GridView;
+use yii\helpers\Html;
+
 ?>
 
 <!-- Блок "ВКР" -->
@@ -23,15 +25,31 @@ use common\modules\GridView\GridView;
                 'dataProvider' => $vkrProvider,
                 'summary' => false,
                 'columns' => [
-                    'title',
+                    [
+                        'attribute' => 'title',
+                        'value' => 'title',
+                        'headerOptions' => ['style' => 'width:30%'],
+                        'contentOptions' => ['style'=>'white-space: normal;']
+                    ],
                     [
                         'attribute' => 'user_id',
                         'label' => 'Научный руководитель',
                         'value' => 'user.fullName',
+                        'headerOptions' => ['style' => 'width:20%'],
                     ],
                     'evaluation',
-                    'comment',
-                    'document'
+                    [
+                        'attribute' => 'comment',
+                        'headerOptions' => ['style' => 'width:15%'],
+                        'contentOptions' => ['style'=>'white-space: normal;']
+                    ],
+                    [
+                        'attribute' => 'document',
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return Html::a('Посмотреть', \common\models\storage\Storage::getFileUrl($model->document), ['target' => '_blank']);
+                        }
+                    ],
                 ],
             ]);
             ?>
@@ -50,11 +68,29 @@ use common\modules\GridView\GridView;
                 'dataProvider' => $cwProvider,
                 'summary' => false,
                 'columns' => [
-                    'title',
-                    'subject',
+                    [
+                        'attribute' => 'title',
+                        'headerOptions' => ['style' => 'width:35%'],
+                        'contentOptions' => ['style'=>'white-space: normal;']
+                    ],
+                    [
+                        'attribute' => 'subject',
+                        'headerOptions' => ['style' => 'width:35%'],
+                        'contentOptions' => ['style'=>'white-space: normal;']
+                    ],
                     'evaluation',
-                    'comment',
-                    'document'
+                    [
+                        'attribute' => 'comment',
+                        'headerOptions' => ['style' => 'width:20%'],
+                        'contentOptions' => ['style'=>'white-space: normal;']
+                    ],
+                    [
+                        'attribute' => 'document',
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return Html::a('Посмотреть', \common\models\storage\Storage::getFileUrl($model->document), ['target' => '_blank']);
+                        }
+                    ],
                 ],
             ]);
             ?>
@@ -73,13 +109,31 @@ use common\modules\GridView\GridView;
                 'dataProvider' => $prProvider,
                 'summary' => false,
                 'columns' => [
-                    'title',
-                    'place',
-                    'date_start',
-                    'date_end',
+                    [
+                        'attribute' => 'title',
+                        'headerOptions' => ['style' => 'width:24%'],
+                        'contentOptions' => ['style'=>'white-space: normal;']
+                    ],
+                    [
+                        'attribute' => 'place',
+                        'headerOptions' => ['style' => 'width:24%'],
+                        'contentOptions' => ['style'=>'white-space: normal;']
+                    ],
+                    'date_start:date',
+                    'date_end:date',
                     'evaluation',
-                    'comment',
-                    'document'
+                    [
+                        'attribute' => 'comment',
+                        'headerOptions' => ['style' => 'width:22%'],
+                        'contentOptions' => ['style'=>'white-space: normal;']
+                    ],
+                    [
+                        'attribute' => 'document',
+                        'format' => 'raw',
+                        'value' => function ($model) {
+                            return Html::a('Посмотреть', \common\models\storage\Storage::getFileUrl($model->document), ['target' => '_blank']);
+                        }
+                    ],
                 ],
             ]);
             ?>
